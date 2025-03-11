2b457c59798bcafb493c081fdbc308f4e8a47802
Neil Traft <ntraft@gmail.com>
2025-03-11 09:40:23 -0400

    Allow both leading and trailing norms.

--- src/adapters.py
+++ src/adapters.py
@@ -10,8 +10,8 @@ class DeRyAdapter(nn.Module):
     This is the adapter as it appeared in Deep Model Reassembly (DeRy).
     From: https://github.com/Adamdad/DeRy/blob/main/mmcls_addon/models/backbones/dery.py
     """
-    def __init__(self, input_channel, output_channel, mode='cnn2cnn', num_fc=0, num_conv=1, stride=1, norm=True,
-                 nonlinearity=True):
+    def __init__(self, input_channel, output_channel, mode='cnn2cnn', num_fc=0, num_conv=1, stride=1, leading_norm=True,
+                 trailing_norm=False, nonlinearity=True):
         super().__init__()
         if (num_fc == 0 and num_conv == 0) or (num_fc > 0 and num_conv > 0):
             raise ValueError('Must supply only num_fc or num_conv.')
@@ -20,18 +20,20 @@ class DeRyAdapter(nn.Module):
 
         layers = []
         if num_fc > 0:
-            if norm:
+            if leading_norm:
                 layers.append(nn.LayerNorm(input_channel))
             for i in range(num_fc):
                 if i == 0:
                     layers.append(nn.Linear(input_channel, output_channel, bias=False))
                 else:
                     layers.append(nn.Linear(output_channel, output_channel, bias=False))
+            if trailing_norm:
+                layers.append(nn.LayerNorm(input_channel))
             if nonlinearity:
                 layers.append(nn.LeakyReLU(0.1, inplace=True))
 
         elif num_conv > 0:
-            if norm:
+            if leading_norm:
                 layers.append(nn.BatchNorm2d(input_channel))
             for i in range(num_conv):
                 if i == 0:
@@ -40,6 +42,8 @@ class DeRyAdapter(nn.Module):
                 else:
                     layers.append(nn.Conv2d(output_channel, output_channel, kernel_size=1, stride=1, padding=0,
                                             bias=False))
+            if trailing_norm:
+                layers.append(nn.BatchNorm2d(input_channel))
             if nonlinearity:
                 layers.append(nn.LeakyReLU(0.1, inplace=True))
 
