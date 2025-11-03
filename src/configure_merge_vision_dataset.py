120b41a0fe8bc8582a5c87e45fa9442c72efb3d8
Neil Traft <ntraft@gmail.com>
2025-11-03 10:41:42 -0500

    New scripts to install vision datasets for model merge benchmark

---
+++ src/configure_merge_vision_dataset.py
@@ -0,0 +1,138 @@
+"""
+A script to configure the datasets downloaded by download-merge-vision-datasets.sh.
+
+These are some of the datasets used by the Task Vectors paper, and subsequent papers, to evaluate model merging. The
+rest of the datasets used by this line of work can be acquired directly through Torchvision, but these few require
+manual setup. These scripts automate most of the setup.
+Task Vectors: https://github.com/mlfoundations/task_vectors/
+Dataset issues: https://github.com/mlfoundations/task_vectors/issues/1
+"""
+import os
+import random
+import shutil
+
+
+base_dir = f'data'
+
+
+### PROCESS SUN397 DATASET
+
+downloaded_data_path = f"{base_dir}/sun397"
+output_path = f"{base_dir}/sun397"
+
+
+def process_dataset(txt_file, downloaded_data_path, output_folder):
+    with open(txt_file, 'r') as file:
+        lines = file.readlines()
+
+    for i, line in enumerate(lines):
+        input_path = line.strip()
+        final_folder_name = "_".join(x for x in input_path.split('/')[:-1])[1:]
+        filename = input_path.split('/')[-1]
+        output_class_folder = os.path.join(output_folder, final_folder_name)
+
+        if not os.path.exists(output_class_folder):
+            os.makedirs(output_class_folder)
+
+        full_input_path = os.path.join(downloaded_data_path, input_path[1:])
+        output_file_path = os.path.join(output_class_folder, filename)
+        # print(final_folder_name, filename, output_class_folder, full_input_path, output_file_path)
+        # exit()
+        shutil.copy(full_input_path, output_file_path)
+        if i % 100 == 0:
+            print(f"Processed {i}/{len(lines)} images")
+
+
+process_dataset(
+    os.path.join(downloaded_data_path, 'Partitions', 'Training_01.txt'),
+    os.path.join(downloaded_data_path, 'SUN397'),
+    os.path.join(output_path, "train")
+)
+process_dataset(
+    os.path.join(downloaded_data_path, 'Partitions', 'Testing_01.txt'),
+    os.path.join(downloaded_data_path, 'SUN397'),
+    os.path.join(output_path, "val")
+)
+
+
+### PROCESS EuroSAT_RGB DATASET
+
+src_dir = f'{base_dir}/euro_sat/2750'  # replace with the path to your dataset
+dst_dir = f'{base_dir}/EuroSAT_splits'  # replace with the path to the output directory
+
+
+def create_directory_structure(dst_dir, classes):
+    for dataset in ['train', 'val', 'test']:
+        path = os.path.join(dst_dir, dataset)
+        os.makedirs(path, exist_ok=True)
+        for cls in classes:
+            os.makedirs(os.path.join(path, cls), exist_ok=True)
+
+
+def split_dataset(dst_dir, src_dir, classes, val_size=270, test_size=270):
+    for cls in classes:
+        class_path = os.path.join(src_dir, cls)
+        images = os.listdir(class_path)
+        random.shuffle(images)
+
+        val_images = images[:val_size]
+        test_images = images[val_size:val_size + test_size]
+        train_images = images[val_size + test_size:]
+
+        for img in train_images:
+            src_path = os.path.join(class_path, img)
+            dst_path = os.path.join(dst_dir, 'train', cls, img)
+            print(src_path, dst_path)
+            shutil.copy(src_path, dst_path)
+            # break
+        for img in val_images:
+            src_path = os.path.join(class_path, img)
+            dst_path = os.path.join(dst_dir, 'val', cls, img)
+            print(src_path, dst_path)
+            shutil.copy(src_path, dst_path)
+            # break
+        for img in test_images:
+            src_path = os.path.join(class_path, img)
+            dst_path = os.path.join(dst_dir, 'test', cls, img)
+            print(src_path, dst_path)
+            shutil.copy(src_path, dst_path)
+            # break
+
+
+classes = [d for d in os.listdir(src_dir) if os.path.isdir(os.path.join(src_dir, d))]
+create_directory_structure(dst_dir, classes)
+split_dataset(dst_dir, src_dir, classes)
+
+
+### PROCESS DTD DATASET
+
+downloaded_data_path = f"{base_dir}/dtd/images"
+output_path = f"{base_dir}/dtd"
+
+
+def process_dataset(txt_file, downloaded_data_path, output_folder):
+    with open(txt_file, 'r') as file:
+        lines = file.readlines()
+
+    for i, line in enumerate(lines):
+        input_path = line.strip()
+        final_folder_name = input_path.split('/')[:-1][0]
+        filename = input_path.split('/')[-1]
+        output_class_folder = os.path.join(output_folder, final_folder_name)
+
+        if not os.path.exists(output_class_folder):
+            os.makedirs(output_class_folder)
+
+        full_input_path = os.path.join(downloaded_data_path, input_path)
+        output_file_path = os.path.join(output_class_folder, filename)
+        shutil.copy(full_input_path, output_file_path)
+        if i % 100 == 0:
+            print(f"Processed {i}/{len(lines)} images")
+
+
+process_dataset(
+    f'{base_dir}/dtd/labels/train.txt', downloaded_data_path, os.path.join(output_path, "train")
+)
+process_dataset(
+    f'{base_dir}/dtd/labels/test.txt', downloaded_data_path, os.path.join(output_path, "val")
+)
---
+++ src/download-merge-vision-datasets.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+###
+# A script to download datasets for vision model merging.
+#
+# NOTE: Before running, manually download this file: https://1drv.ms/u/s!AmgKYzARBl5ca3HNaHIlzp_IXjs
+#
+# These are some of the datasets used by the Task Vectors paper, and subsequent papers, to evaluate model merging. The
+# rest of the datasets used by this line of work can be acquired directly through Torchvision, but these few require
+# manual setup. These scripts automate most of the setup.
+# Task Vectors: https://github.com/mlfoundations/task_vectors/
+# Dataset issues: https://github.com/mlfoundations/task_vectors/issues/1
+###
+
+# Exit immediately if any command has a non-zero return code.
+set -e
+set -o pipefail
+
+pip install kaggle
+cd data
+export KAGGLE_USERNAME=neiltraft
+export KAGGLE_KEY=550514a731a19c14bdcf685830db9801
+
+# stanford cars dataset (ref: https://github.com/pytorch/vision/issues/7545#issuecomment-2282674373)
+mkdir -p stanford_cars
+if [ -z "$(ls -A stanford_cars)" ]; then  # Only proceed if directory is empty.
+  echo "Downloading Stanford Cars"
+  kaggle datasets download --unzip rickyyyyyyy/torchvision-stanford-cars
+else
+  echo "Skipping Stanford Cars"
+fi
+
+# resisc45
+mkdir -p resisc45
+if [ -z "$(ls -A resisc45)" ]; then  # Only proceed if directory is empty.
+  echo "Downloading RESISC45"
+  cd resisc45
+  # NOTE: Manually download this before running: https://1drv.ms/u/s!AmgKYzARBl5ca3HNaHIlzp_IXjs
+  mv ~/Downloads/NWPU-RESISC45.rar ./
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    brew install unar
+  else
+    sudo apt -y install unar
+  fi
+  unar NWPU-RESISC45.rar
+  wget -O resisc45-train.txt "https://huggingface.co/datasets/torchgeo/resisc45/resolve/main/resisc45-train.txt"
+  wget -O resisc45-val.txt "https://huggingface.co/datasets/torchgeo/resisc45/resolve/main/resisc45-val.txt"
+  wget -O resisc45-test.txt "https://huggingface.co/datasets/torchgeo/resisc45/resolve/main/resisc45-test.txt"
+  rm -rf NWPU-RESISC45.rar
+  cd ..
+else
+  echo "Skipping RESISC45"
+fi
+
+# dtd
+mkdir -p dtd
+if [ -z "$(ls -A dtd)" ]; then  # Only proceed if directory is empty.
+  echo "Downloading DTD"
+  cd dtd
+  wget https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz
+  tar -xvzf dtd-r1.0.1.tar.gz
+  rm -rf dtd-r1.0.1.tar.gz
+  mv dtd/images images
+  mv dtd/imdb/ imdb
+  mv dtd/labels labels
+  cat labels/train1.txt labels/val1.txt > labels/train.txt
+  cat labels/test1.txt > labels/test.txt
+  cd ..
+else
+  echo "Skipping DTD"
+fi
+
+# euro_sat
+mkdir -p euro_sat
+if [ -z "$(ls -A euro_sat)" ]; then  # Only proceed if directory is empty.
+  echo "Downloading EuroSAT"
+  cd euro_sat
+  wget --no-check-certificate https://madm.dfki.de/files/sentinel/EuroSAT.zip
+  unzip EuroSAT.zip
+  rm -rf EuroSAT.zip
+  cd ..
+else
+  echo "Skipping EuroSAT"
+fi
+
+# sun397
+# NOTE: SUN397 is no longer available from the original host. Instead, manually download and extract it using the
+# torrent at this link: https://hyper.ai/en/datasets/5367
+#mkdir sun397
+#if [ -z "$(ls -A sun397)" ]; then  # Only proceed if directory is empty.
+#  echo "Downloading SUN397"
+#  cd sun397
+#  wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
+#  unzip Partitions.zip
+#  tar -xvzf SUN397.tar.gz
+#  rm -rf SUN397.tar.gz
+#else
+#  echo "Skipping SUN397"
+#fi
