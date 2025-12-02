55d0e1de9724223fc3265e82888247fac234f6e9
Neil Traft <ntraft@gmail.com>
2025-12-02 10:30:36 -0600

    Pathify dataset prep script.

--- src/configure_merge_vision_dataset.py
+++ src/configure_merge_vision_dataset.py
@@ -7,71 +7,73 @@ manual setup. These scripts automate most of the setup.
 Task Vectors: https://github.com/mlfoundations/task_vectors/
 Dataset issues: https://github.com/mlfoundations/task_vectors/issues/1
 """
-import os
 import random
 import shutil
+from pathlib import Path
+
+from tqdm import tqdm
 
 
-base_dir = f'data'
+base_dir = Path("data")
 
 
 ### PROCESS SUN397 DATASET
+print("Processing SUN397...")
 
-downloaded_data_path = f"{base_dir}/sun397"
-sun_dir = f"{base_dir}/sun397"
+downloaded_data_path = base_dir / "sun397"  # replace with the path to your dataset
+sun_dir = base_dir / "sun397"  # replace with the path to the output directory
 
 
 def process_dataset(txt_file, downloaded_data_path, output_folder):
+    print(f"Moving files from {txt_file.name} into {output_folder}...")
     with open(txt_file, 'r') as file:
         lines = file.readlines()
 
-    for i, line in enumerate(lines):
+    for line in tqdm(lines):
         input_path = line.strip()
         final_folder_name = "_".join(x for x in input_path.split('/')[:-1])[1:]
         filename = input_path.split('/')[-1]
-        output_class_folder = os.path.join(output_folder, final_folder_name)
+        output_class_folder = output_folder / final_folder_name
 
-        if not os.path.exists(output_class_folder):
-            os.makedirs(output_class_folder)
+        if not output_class_folder.exists():
+            output_class_folder.mkdir()
 
-        full_input_path = os.path.join(downloaded_data_path, input_path[1:])
-        output_file_path = os.path.join(output_class_folder, filename)
+        full_input_path = downloaded_data_path / input_path[1:]
+        output_file_path = output_class_folder / filename
         # print(final_folder_name, filename, output_class_folder, full_input_path, output_file_path)
         # exit()
         shutil.copy(full_input_path, output_file_path)
-        if i % 100 == 0:
-            print(f"Processed {i}/{len(lines)} images")
 
 
 process_dataset(
-    os.path.join(downloaded_data_path, 'Partitions', 'Training_01.txt'),
-    os.path.join(downloaded_data_path, 'SUN397'),
-    os.path.join(sun_dir, "train")
+    downloaded_data_path / 'Partitions' / 'Training_01.txt',
+    downloaded_data_path / 'SUN397',
+    sun_dir / "train"
 )
 process_dataset(
-    os.path.join(downloaded_data_path, 'Partitions', 'Testing_01.txt'),
-    os.path.join(downloaded_data_path, 'SUN397'),
-    os.path.join(sun_dir, "val")
+    downloaded_data_path / 'Partitions' / 'Testing_01.txt',
+    downloaded_data_path / 'SUN397',
+    sun_dir / "val"
 )
 
 
 ### PROCESS EuroSAT_RGB DATASET
 
-src_dir = f'{base_dir}/eurosat/2750'  # replace with the path to your dataset
-dst_dir = f'{base_dir}/eurosat_splits'  # replace with the path to the output directory
+src_dir = base_dir / "eurosat" / "2750"  # replace with the path to your dataset
+dst_dir = base_dir / "eurosat_splits"  # replace with the path to the output directory
 
 
 def create_directory_structure(dst_dir, classes):
     for dataset in ['train', 'val', 'test']:
-        path = os.path.join(dst_dir, dataset)
-        os.makedirs(path, exist_ok=True)
+        path = dst_dir / dataset
+        path.mkdir(exist_ok=True)
         for cls in classes:
-            os.makedirs(os.path.join(path, cls), exist_ok=True)
+            (path / cls).mkdir(exist_ok=True)
 
 
 def split_dataset(dst_dir, src_dir, classes, val_size=270, test_size=270):
     for cls in classes:
-        class_path = os.path.join(src_dir, cls)
+        class_path = src_dir / cls
         images = os.listdir(class_path)
         random.shuffle(images)
 
@@ -80,25 +82,25 @@ def split_dataset(dst_dir, src_dir, classes, val_size=270, test_size=270):
         train_images = images[val_size + test_size:]
 
         for img in train_images:
-            src_path = os.path.join(class_path, img)
-            dst_path = os.path.join(dst_dir, 'train', cls, img)
+            src_path = class_path / img
+            dst_path = dst_dir / "train" / cls / img
             print(src_path, dst_path)
             shutil.copy(src_path, dst_path)
             # break
         for img in val_images:
-            src_path = os.path.join(class_path, img)
-            dst_path = os.path.join(dst_dir, 'val', cls, img)
+            src_path = class_path / img
+            dst_path = dst_dir / "val" / cls / img
             print(src_path, dst_path)
             shutil.copy(src_path, dst_path)
             # break
         for img in test_images:
-            src_path = os.path.join(class_path, img)
-            dst_path = os.path.join(dst_dir, 'test', cls, img)
+            src_path = class_path / img
+            dst_path = dst_dir / "test" / cls / img
             print(src_path, dst_path)
             shutil.copy(src_path, dst_path)
             # break
 
 
-classes = [d for d in os.listdir(src_dir) if os.path.isdir(os.path.join(src_dir, d))]
+classes = [d for d in src_dir.iterdir() if d.is_dir()]
 create_directory_structure(dst_dir, classes)
 split_dataset(dst_dir, src_dir, classes)
